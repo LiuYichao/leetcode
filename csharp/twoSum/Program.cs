3fec9737b43fe3cb5aa8523260145129b8ddb7c6
liuyichao <liuyichao1207@gmail.com>
2020-03-03 00:01:07 +0900

    add two sum solution(C# version)

--- csharp/twoSum/Program.cs
+++ csharp/twoSum/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace twoSum
 {
-    class Program
+    class TwoSum
     {
-        static void Main(string[] args)
+        public int[] bruteForce(int[] nums, int target)
         {
-            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+            throw new Exception("No two sum solution");
+        }
+
+        public int[] twoPassHashTable(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dic.Add(nums[i], i);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (dic.TryGetValue(complement, out index) && index != i)
+                {
+                    return new int[] { index, i };
+                }
+
+            }
+            throw new Exception("No two sum solution");
+        }
+
+
+        public int[] onePassHashTable(int[] nums, int target)
+        {
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int complement = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                complement = target - nums[i];
+                if (dic.ContainsKey(complement))
+                {
+                    return new int[] { i, dic[complement] };
+                }
+                else
+                {
+                    dic[nums[i]] = i;
+                }
+            }
+            throw new Exception("No two sum solution");
         }
     }
 }
